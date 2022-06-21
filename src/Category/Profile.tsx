953e8db8d2341f7c72dab278d75916ec166a7a0f
kaito071831 <kaito071831@gmail.com>
2022-06-21 16:14:46 +0900

    作成中

---
+++ src/Category/Profile.tsx
@@ -0,0 +1,34 @@
+import { Contents } from "../components/content"
+
+const ICON = require('../../public/image/my_icon.jpg')
+
+const profiles = [
+  {
+    label: "Name",
+    content: "酒尾 快都 / Kaito Sakao"
+  },
+  {
+    label: "From",
+    content: "石川県金沢市"
+  },
+  {
+    label: "Write",
+    content: "Java/Python/Ruby/JavaScript(TypeScript)/Go"
+  },
+  {
+    label: "Hobby",
+    content: "アニメ鑑賞/カラオケ/サイクリング"
+  },
+  {
+    label: "Contact",
+    content: "kaito071831@gmail.com"
+  }
+]
+
+export const Profile = () => {
+  return(
+    <Contents bgColor="#666666">
+      Test
+    </Contents>
+  )
+}
