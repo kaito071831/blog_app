aea500b2f5010056f3e7cb63298640984edd89c7
kaito071831 <kaito071831@gmail.com>
2022-06-23 22:35:55 +0900

    インポート元の変更

--- src/pages/blog/[id].tsx
+++ src/pages/blog/[id].tsx
@@ -1,4 +1,4 @@
-import { client } from "../libs/client";
+import { client } from "../../libs/client";
 import { GetStaticPaths, NextPage } from "next";
 import { Blog, Context } from "../../types/blog";
 import { MicroCMSListResponse } from "microcms-js-sdk";
