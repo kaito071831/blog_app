ddf9c5770f90b6e7a6a78ad45e914f5257aea5bd
kaito071831 <kaito071831@gmail.com>
2022-06-23 17:06:09 +0900

    作成

---
+++ src/pages/_document.tsx
@@ -0,0 +1,15 @@
+import { Html, Main, NextScript } from "next/document";
+import Head from "next/head";
+
+const Document = () => {
+  return(
+    <Html lang="ja">
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
+
+export default Document;
