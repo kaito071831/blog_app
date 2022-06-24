68734c1ac7693f0e2e2d559817bc0e1c83bdacac
kaito071831 <kaito071831@gmail.com>
2022-06-24 13:40:01 +0900

    Styled-componentをSSRに対応

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,20 @@
 import '../styles/globals.css'
 import { NextPage } from 'next'
 import type { AppProps } from 'next/app'
+import { useEffect } from 'react';
 
-const App: NextPage<AppProps> = ({ Component, pageProps }: AppProps) =>  {
-  return <Component {...pageProps} />
+const App: NextPage<AppProps> = ({ Component, pageProps }: AppProps): JSX.Element =>  {
+  useEffect(() => {
+    const jssStyles: Element | null =
+    document.querySelector("#jss-server-side");
+    if (jssStyles) {
+    jssStyles.parentElement?.removeChild(jssStyles);
+    }
+    }, []);
+
+  return(
+    <Component {...pageProps} />
+  )
 }
 
 export default App
