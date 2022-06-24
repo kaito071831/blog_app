dde63bf643646b2339720ae52275b897eb82e882
kaito071831 <kaito071831@gmail.com>
2022-06-24 13:39:40 +0900

    fix

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,7 @@ const Document = () => {
   return(
     <Html lang="ja">
       <Head>
-      <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP&family=Rubik+Moonrocks&display=swap" rel="stylesheet"/>
+        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP&family=Rubik+Moonrocks&display=swap" rel="stylesheet"/>
       </Head>
       <body>
         <Main />
