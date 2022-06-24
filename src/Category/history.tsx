2149008b5f3926d873272c7896da45eaf10ec6df
kaito071831 <kaito071831@gmail.com>
2022-06-24 13:28:06 +0900

    内容をオブジェクト配列にした。

--- src/Category/history.tsx
+++ src/Category/history.tsx
@@ -45,56 +45,57 @@ const HistoryDiv = styled.div`
   }
 `
 
+const careers = [
+  {
+    date: "2016/04-2021/03",
+    content: "国際高等専門学校 グローバル情報学科",
+    detail: "プログラミングの基礎, 会計の基礎を学習",
+  },
+  {
+    date: "2019/08",
+    content: "NTT-ME インターン",
+    detail: "電話・インターネット回線の構築の疑似体験",
+  },
+  {
+    date: "2021/04-",
+    content: "金沢工業大学 工学部 情報工学科",
+    detail: "コンピューターやシステム開発について学習"
+  },
+  {
+    date: "2021/04-",
+    content: "株式会社CirKit Webエンジニア インターン",
+    detail: "システム開発部にてインターン。金沢工業大学の学食で利用されているサービスSAKITOの開発,HPの保守に従事。",
+    tech: "Ruby / Ruby on Rails / Python / JavaScript(TypeScript) / React",
+  },
+  {
+    date: "2021/08",
+    content: "株式会社TSOne エンジニア インターン",
+    detail: "来客者受付システムの開発",
+    tech: "Python / Flask",
+  },
+  {
+    date: "2021/09",
+    content: "株式会社タスク エンジニア インターン",
+    detail: "コッペパン購入システムの開発",
+    tech: "JavaScript / GoogleAppsScript",
+  },
+]
+
 export const History = () => {
   return(
     <Contents bgColor="#d4ae69">
       <HistoryH2>History</HistoryH2>
       <div>
-        <HistoryContent>
-          <HistoryP>2016/04-2021/03</HistoryP>
-          <HistoryDiv>
-            <h3>国際高等専門学校 グローバル情報学科</h3>
-            <p>プログラミングの基礎, 会計の基礎を学習</p>
-          </HistoryDiv>
-        </HistoryContent>
-        <HistoryContent>
-          <HistoryP>2019/08</HistoryP>
-          <HistoryDiv>
-            <h3>NTT-ME インターン</h3>
-            <p>電話・インターネット回線の構築の疑似体験</p>
-          </HistoryDiv>
-        </HistoryContent>
-        <HistoryContent>
-          <HistoryP>2021/04-</HistoryP>
-          <HistoryDiv>
-            <h3>金沢工業大学 情報工学科</h3>
-            <p>コンピューターやシステム開発について学習</p>
-          </HistoryDiv>
-        </HistoryContent>
-        <HistoryContent>
-          <HistoryP>2021/04-</HistoryP>
-          <HistoryDiv>
-            <h3>株式会社CirKit Webエンジニア インターン</h3>
-            <p>システム開発部にてインターン。金沢工業大学の学食で利用されているサービスSAKITOの開発に従事</p>
-            <p>Ruby / Ruby on Rails / Python</p>
-          </HistoryDiv>
-        </HistoryContent>
-        <HistoryContent>
-          <HistoryP>2021/08</HistoryP>
-          <HistoryDiv>
-            <h3>株式会社TSOne エンジニア インターン</h3>
-            <p>来客者受付システムの開発</p>
-            <p>Python / Flask</p>
-          </HistoryDiv>
-        </HistoryContent>
-        <HistoryContent>
-          <HistoryP>2021/09</HistoryP>
-          <HistoryDiv>
-            <h3>株式会社タスク エンジニア インターン</h3>
-            <p>コッペパン購入システムの開発</p>
-            <p>JavaScript / GoogleAppsScript</p>
-          </HistoryDiv>
-        </HistoryContent>
+        { careers.map((career) => (
+          <HistoryContent>
+            <HistoryP>{career.date}</HistoryP>
+            <HistoryDiv>
+              <h3>{career.content}</h3>
+              <p>{career.detail}</p>
+              <p>{career.tech}</p>
+            </HistoryDiv>
+          </HistoryContent>
+        ))}
       </div>
     </Contents>
   )
