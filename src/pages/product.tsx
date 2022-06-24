b61bb06f303b51700819fd6f3752bc8c0abe162a
kaito071831 <kaito071831@gmail.com>
2022-06-24 17:40:47 +0900

    productページ作成

---
+++ src/pages/product.tsx
@@ -0,0 +1,17 @@
+import { NextPage } from "next";
+import { Contents } from "../components/content";
+import { Layout } from "../components/layout";
+
+const Product: NextPage = () => {
+  return(
+    <>
+      <Layout title="Product | kaito071831">
+        <Contents bgColor="#2fdc2f">
+          
+        </Contents>
+      </Layout>
+    </>
+  )
+}
+
+export default Product;
