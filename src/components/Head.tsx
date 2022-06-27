251c7b8879d5914cf75906a87df1af2b325eb288
kaito071831 <kaito071831@gmail.com>
2022-06-28 02:58:54 +0900

    imageのソースを追加

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -18,7 +18,7 @@ export const HeadParts = (props: Page) => {
       <meta property="og:title" content={title}/>
       <meta property="og:description" content={DESCRIPTION}/>
       <meta property="og:site_name" content={HP_TITLE}/>
-      <meta property="og:image" content=""/>
+      <meta property="og:image" content={`${process.env.HP_URL}/favicon.ico`}/>
       <meta name="twitter:card" content="summary"/>
       <meta name="twitter:site" content="@kaito071831"/>
     </Head>
