b93f59af5f529b7a027ec384c2c8576683ca6fbe
kaito071831 <kaito071831@gmail.com>
2022-06-28 02:54:45 +0900

    Headを作成

---
+++ src/components/Head.tsx
@@ -0,0 +1,26 @@
+import Head from "next/head";
+
+type Page = {
+  title?: string;
+}
+
+const HP_TITLE = "kaito071831"
+const DESCRIPTION = "kaito071831のHPです"
+
+export const HeadParts = (props: Page) => {
+  const title: string = props.title ? `${props.title} | ${HP_TITLE}` : HP_TITLE
+  return(
+    <Head>
+      <title>{title}</title>
+      <meta name="viewport" content="width=device-width,initial-scale=1"/>
+      <meta property="og:url" content={process.env.HP_URL}/>
+      <meta property="og:type" content="website"/>
+      <meta property="og:title" content={title}/>
+      <meta property="og:description" content={DESCRIPTION}/>
+      <meta property="og:site_name" content={HP_TITLE}/>
+      <meta property="og:image" content=""/>
+      <meta name="twitter:card" content="summary"/>
+      <meta name="twitter:site" content="@kaito071831"/>
+    </Head>
+  )
+}
