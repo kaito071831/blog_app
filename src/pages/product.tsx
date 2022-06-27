f6bec698a6c88b7d487212ac26591f73cada0c0c
kaito071831 <kaito071831@gmail.com>
2022-06-27 17:05:41 +0900

    ページ作成

--- src/pages/product.tsx
+++ src/pages/product.tsx
@@ -4,11 +4,12 @@ import Image from "next/image";
 import { Contents } from "../components/content";
 import { Layout } from "../components/layout";
 import styled from "styled-components";
+import { pc, sp } from "../styles/media";
 
-const KAITO_HP = require('/public/image/my_site.png');
-const TASK_CONTROL = require('/public/image/task-control-app.herokuapp.com_.png');
-const KTimer = require('/public/image/KTimer.png');
-const WORK_APP = require('/public/image/work_app.png');
+const KAITO_HP = require('../../public/image/my_site.png');
+const TASK_CONTROL = require('../../public/image/task-control-app.herokuapp.com_.png');
+const KTimer = require('../../public/image/KTimer.png');
+const WORK_APP = require('../../public/image/work_app.png');
 
 const ProductIndex = styled.div`
   display: flex;
@@ -17,10 +18,66 @@ const ProductIndex = styled.div`
 `
 
 const ProductContent = styled.div`
-  border: 0.25rem #000000 solid;
+  border: 0.5rem #000000 double;
   border-radius: 1rem;
   margin: 1rem auto;
-  padding: 1rem;
+  padding: 1.75rem;
+`
+
+const ProductLink = styled.a`
+  cursor: pointer;
+` 
+
+const ProductSpan = styled.span`
+  ${pc`
+    font-size: 1rem;
+  `}
+  ${sp`
+    font-size: 0.75rem;
+  `}
+`
+
+const ProductDt = styled.dt`
+  float: left;
+  clear: left;
+  margin: 0.25rem 0;
+  font-family: 'Rubik Moonrocks', cursive;
+  ${sp`
+    width: 100%;
+  `}
+  ${pc`
+    width: 6rem;
+  `}
+`
+
+const ProductDd = styled.dd`
+  float: left;
+  margin: 0.25rem 0;
+  font-family: 'Noto Sans JP', sans-serif;
+  ${sp`
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    width: 100%;
+  `}
+`
+
+const ProductTech = styled.div`
+  display: inline-block;
+  border: 0.1rem #000000 none;
+  border-radius: 0.25rem;
+  color: #1d90ed;
+  background-color: #e4e4e4;
+  padding: 0.1rem;
+  ${pc`
+    display: inline-block;
+    margin-right: 0.5rem;
+  `}
+  ${sp`
+    display: block;
+    margin-right: 0.5rem;
+    margin-bottom: 0.5rem;
+  `}
 `
 
 const products = [
@@ -28,6 +85,8 @@ const products = [
     title: "ポートフォリオサイト",
     url: "https://github.com/kaito071831",
     src: KAITO_HP,
+    height: 0,
+    width: 0,
     detail: "自分のポートフォリオサイト",
     tech: ['TypeScript', 'Next.js', 'React', 'Vercel', 'microCMS'],
   },
@@ -35,6 +94,8 @@ const products = [
     title: "TaskControl",
     url: "https://task-control-app.herokuapp.com/",
     src: TASK_CONTROL,
+    height: 0,
+    width: 0,
     detail: "ToDo管理サービス",
     tech: ['Ruby', 'Ruby on Rails', 'Tailwind CSS', 'jQuery', 'Heroku'],
   },
@@ -42,6 +103,8 @@ const products = [
     title: "KTimer",
     url: "https://github.com/kaito071831/Practice",
     src: KTimer,
+    height: 300,
+    width: 600,
     detail: "キッチンタイマー",
     tech: ['Java'],
   },
@@ -49,6 +112,8 @@ const products = [
     title: "業務用メモ",
     url: "https://github.com/kaito071831/MultipleMemo",
     src: WORK_APP,
+    height: 370,
+    width: 270,
     detail: "商店・飲食店向け記録用メモアプリケーション",
     tech: ['Java'],
   },
@@ -57,24 +122,41 @@ const products = [
 const Product: NextPage = () => {
   return(
     <>
-      <Layout title="Product | kaito071831">
-        <Contents bgColor="#2fdc2f">
+      <Layout title='Product | kaito071831'>
+        <Contents bgColor="#74b3e7">
+          <h1>Product</h1>
           <ProductIndex>
-            {products.map((product) => (
-              <ProductContent>
+            {products.map((product, index) => (
+              <ProductContent key={index}>
                 <div>
-                  <Link href={product.url}>
-                    <Image
-                      src={product.src}
-                    />
+                  <h2>{product.title}</h2>
+                  <Link href={product.url} passHref>
+                    <ProductLink>
+                      <Image
+                        src={product.src}
+                        height={product.height != 0 ? product.height : 0}
+                        width={product.width != 0 ? product.width : 0}
+                      />
+                    </ProductLink>
                   </Link>
                 </div>
                 <div>
-                  <p>{product.title}</p>
-                  <p>{product.detail}</p>
-                  {product.tech.map((tc) => (
-                    <p>{tc}</p>
-                  ))}
+                  <dl>
+                    <ProductSpan>
+                      <ProductDt>説明</ProductDt>
+                      <ProductDd>{product.detail}</ProductDd>
+                    </ProductSpan>
+                    <ProductSpan>
+                      <ProductDt>使用技術</ProductDt>
+                      <ProductDd>
+                        {product.tech.map((tc, index) => (
+                          <ProductTech key={index}>
+                            <span>{tc}</span>
+                          </ProductTech>
+                        ))}
+                      </ProductDd>
+                    </ProductSpan>
+                  </dl>
                 </div>
               </ProductContent>
             ))}
